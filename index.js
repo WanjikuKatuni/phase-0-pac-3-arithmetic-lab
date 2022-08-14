2ed862e3a3994899dc4125d6492c52f15eaa3868
wanjiku <annritawanjiku77@gmail.com>
2022-08-15 01:17:19 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a,b){
+    return a+b;
+}
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
+function increment(a){
+    return a+1;
+}
+function decrement(a){
+    return a-1;
+}
+
+function makeInt(a){
+    return a.toString();
+
+}
+function makeInt(a){
+    return parseInt(a,10);
+}
+
+
+
+function preserveDecimal(a){
+    return a.toFixed(3);
+}
+
+function preserveDecimal(a){
+    return parseFloat(a);
+}
